696f700afd97765690416f1dc091209a459833d1
Ben Weymouth <benjamin.weymouth@gmail.com>
2020-01-26 16:28:06 -0500

    added IFFY to js

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,5 +3,10 @@
 //IIFE - Immediately Invoked Function Expression
 // anonymous self excecuting function
 (function(){
-  
+
+ function Start() {
+   console.log("App started!");
+ }
+ window.onload=Start;
+ //window loads and then the start function is called. 
 })();
